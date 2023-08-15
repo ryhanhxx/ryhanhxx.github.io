9a215bf065a31305249150d681e953a893361498
Muhammad Rayhan Hakim <99849780+ryhanhxx@users.noreply.github.com>
2023-08-15 12:47:13 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+function toggleImageDisplayUi() {
+    var x = document.getElementById("idImg1");
+    var y = document.getElementById("idImg2");
+    var z = document.getElementById("idImg3");
+
+    if (x.style.display === "none") {
+        
+    } else {
+        y.style.display = "none";
+        x.style.display = "none";
+        z.style.display = "flex";
+    }
+}
+
+function toggleImageDisplayVec(){
+    var x = document.getElementById("idImg1");
+    var y = document.getElementById("idImg2");
+    var z = document.getElementById("idImg3");
+
+    if (z.style.display === "none") {
+        
+    } else {
+        y.style.display = "flex";
+        x.style.display = "flex";
+        z.style.display = "none";
+    }
+}
+
