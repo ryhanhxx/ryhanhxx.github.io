e32528c726bff2775353a61f3c1bb154357b1cc3
Muhammad Rayhan Hakim <99849780+ryhanhxx@users.noreply.github.com>
2023-12-18 12:22:01 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 function toggleImageDisplayUi() {
     var x = document.getElementById("idImg1");
-    var y = document.getElementById("idImg2");
+    // var y = document.getElementById("idImg2");
     var z = document.getElementById("idImg3");
 
-    if (x.style.display === "none" || y.style.display === "none") {
-        
+    // if (x.style.display === "none" || y.style.display === "none") {
+    if (x.style.display === "none" ){ 
     } else {
-        y.style.display = "none";
+        // y.style.display = "none";
         x.style.display = "none";
         z.style.display = "flex";
     }
@@ -14,13 +14,13 @@ function toggleImageDisplayUi() {
 
 function toggleImageDisplayVec(){
     var x = document.getElementById("idImg1");
-    var y = document.getElementById("idImg2");
+    // var y = document.getElementById("idImg2");
     var z = document.getElementById("idImg3");
 
     if (z.style.display === "none") {
         
     } else {
-        y.style.display = "flex";
+        // y.style.display = "flex";
         x.style.display = "flex";
         z.style.display = "none";
     }
