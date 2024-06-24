fe99ec26ede0b65c1f52b3d10b2f045056471b81
Muhammad Rayhan Hakim <99849780+ryhanhxx@users.noreply.github.com>
2024-06-24 12:22:59 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,32 +1,33 @@
 function toggleImageDisplayUi() {
     var x = document.getElementById("idImg1");
-    // var y = document.getElementById("idImg2");
+    var y = document.getElementById("idImg2");
     var z = document.getElementById("idImg3");
     var x1 = document.getElementById("idImg4");
 
     // if (x.style.display === "none" || y.style.display === "none") {
-    if (x.style.display === "none" ){ 
+    if (z.style.display === "none"){ 
+
     } else {
-        // y.style.display = "none";
-        x.style.display = "none";
-        z.style.display = "flex";
-        x1.style.display = "flex";
+        y.style.display = "flex";
+        x.style.display = "flex";
+        z.style.display = "none";
+        x1.style.display = "none";
     }
 }
 
 function toggleImageDisplayVec(){
     var x = document.getElementById("idImg1");
-    // var y = document.getElementById("idImg2");
+    var y = document.getElementById("idImg2");
     var z = document.getElementById("idImg3");
     var x1 = document.getElementById("idImg4");
 
-    if (z.style.display === "none") {
+    if (x.style.display === "none") {
         
     } else {
-        // y.style.display = "flex";
-        x.style.display = "flex";
-        z.style.display = "none";
-        x1.style.display = "none";
+        y.style.display = "none";
+        x.style.display = "none";
+        z.style.display = "flex";
+        x1.style.display = "flex";
     }
 }
 
